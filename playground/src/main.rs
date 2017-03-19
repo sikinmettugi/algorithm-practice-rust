c9608aa4a20a2de68511d214515f34d70c3c6336
Chang-jae Lee <cjlee@Chang-jaeui-MacBook-Pro.local>
2017-03-19 16:30:02 +0900

    implement BOJ-11718
    This commit
    - implements Bakjoon Online Judge Problem #11718
    - implements a crate named boj1 for BOJ Problem set 1.

---
+++ playground/src/main.rs
@@ -0,0 +1,14 @@
+/// Playground
+/// http://stackoverflow.com/questions/25632070/reading-in-user-input-from-console-once-efficiently
+
+extern crate boj1;
+use boj1::*;
+
+fn main() {
+    println!("Hello, world!");
+
+    read_console_then_echo();
+}
+
+
+
---
+++ playground/target/debug/deps/playground-d73744601de83e79.d
@@ -0,0 +1,5 @@
+/Users/cjlee/Practices/RustPractice/AlgorithmPractices/algorithm-practice-rust/playground/target/debug/deps/playground-d73744601de83e79: src/main.rs
+
+/Users/cjlee/Practices/RustPractice/AlgorithmPractices/algorithm-practice-rust/playground/target/debug/deps/playground-d73744601de83e79.d: src/main.rs
+
+src/main.rs:
